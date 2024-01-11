731540f37f091f5d88ec5424538c7f9bec8fbec5
새미 <s22058@gsm.hs.kr>
2024-01-11 20:27:20 +0900

    test

--- Projects/Feature/Sources/Scene/Main/View/MainViewController.swift
+++ Projects/Feature/Sources/Scene/Main/View/MainViewController.swift
@@ -11,7 +11,19 @@ import UIKit
 public final class MainViewController: BaseViewController {
     
     // MARK: - Properties
-    private let profileView = UIView()
+    private let logo = UIImageView(image: .image.gomsLogo.image)
+    
+    private let settingButton = UIButton().then {
+        $0.setBackgroundImage(.image.gomsSettingIcon.image, for: .normal)
+    }
+    
+    private let profileView = UIView().then {
+        $0.backgroundColor = .color.gomsTertiary.color
+        $0.clipsToBounds = true
+        $0.layer.cornerRadius = 12
+        $0.layer.borderWidth = 1
+        $0.layer.borderColor = UIColor.color.gomsSecondary.color.cgColor
+    }
     
     let profileImageView = UIImageView()
     
@@ -63,16 +75,31 @@ public final class MainViewController: BaseViewController {
     // MARK: - Life Cycle
     public override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .color.gomsNegative.color
+        navigationController?.navigationBar.barTintColor = UIColor.systemPink
+        view.backgroundColor = .color.gomsBackground.color
     }
     
     // MARK: - Add View
     override func addView() {
-
+        [logo, settingButton, profileView].forEach { view.addSubview($0) }
     }
     
     // MARK: - Layout
     override func setLayout() {
- 
+        logo.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide)
+            $0.leading.equalToSuperview().inset(20)
+        }
+        
+        settingButton.snp.makeConstraints {
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(16.86)
+            $0.trailing.equalToSuperview().inset(20.92)
+        }
+        
+        profileView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.top.equalTo(logo.snp.bottom).offset(32)
+            $0.height.equalTo(96)
+        }
     }
 }
