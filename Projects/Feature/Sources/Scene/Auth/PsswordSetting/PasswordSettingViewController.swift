d4d65ec978e20e02dc419ab314260b4dba524a3d
새미 <s22058@gsm.hs.kr>
2024-03-28 17:03:12 +0900

    💄 :: PasswordSetting Publishing

--- Projects/Feature/Sources/Scene/Auth/PsswordSetting/PasswordSettingViewController.swift
+++ Projects/Feature/Sources/Scene/Auth/PsswordSetting/PasswordSettingViewController.swift
@@ -8,11 +8,70 @@
 
 import UIKit
 
-public final class PasswordSettingViewController: UIViewController {
+public final class PasswordSettingViewController: BaseViewController {
 
-
+    // MARK: - Properties
+    private let textFieldStackView = UIStackView().then {
+        $0.spacing = 32
+        $0.axis = .vertical
+        $0.distribution = .fillEqually
+        $0.alignment = .fill
+    }
+    
+    private let passwordTextField = GOMSTextField(frame: CGRect(x: 0, y: 0, width: 0, height: 0), placeholder: "비밀번호")
+    
+    private let checkPasswordTextField = GOMSTextField(frame: CGRect(x: 0, y: 0, width: 0, height: 0), placeholder: "비밀번호 확인")
+    
+    private let conditionsLabel = UILabel().then {
+        $0.text = "대/소문자, 특수문자 포함 12자 이상"
+        $0.font = .pretendard(size: 16, weight: .regular)
+        $0.textColor = .color.gomsTertiary.color
+    }
+    
+    private let signUpButton = GOMSButton(frame: CGRect(x: 0, y: 0, width: 0, height: 0), title: "회원가입")
+    
     // MARK: - Life Cycel
     public override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    // MARK: - Navigation
+    override func configNavigation() {
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationItem.title = "비밀번호 설정"
+    }
+    
+    // MARK: - Add View
+    override func addView() {
+        [passwordTextField, checkPasswordTextField].forEach { textFieldStackView.addArrangedSubview($0) }
+        [textFieldStackView, conditionsLabel, signUpButton].forEach { view.addSubview($0) }
+    }
+    
+    // MARK: - Layout
+    override func setLayout() {
+        passwordTextField.snp.makeConstraints {
+            $0.height.equalTo(64)
+        }
+        
+        checkPasswordTextField.snp.makeConstraints {
+            $0.height.equalTo(64)
+        }
+        
+        textFieldStackView.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.top.equalToSuperview().inset(237)
+        }
+        
+        conditionsLabel.snp.makeConstraints {
+            $0.height.equalTo(48)
+            $0.top.equalTo(textFieldStackView.snp.bottom)
+            $0.leading.equalToSuperview().inset(28)
+        }
+        
+        signUpButton.snp.makeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalToSuperview().inset(138)
+            $0.height.equalTo(48)
+        }
     }
 }
