c9579a662d153c427e6334c705a2c76a8afdd054
새미 <s22058@gsm.hs.kr>
2024-03-31 00:09:24 +0900

    ✨ :: Add RefreshTokenRequest

---
+++ Projects/Service/Sources/Request/Auth/RefreshToken/RefreshTokenRequest.swift
@@ -0,0 +1,13 @@
+//
+//  RefreshTokenRequest.swift
+//  Service
+//
+//  Created by 새미 on 3/31/24.
+//  Copyright © 2024 HARIBO. All rights reserved.
+//
+
+import Foundation
+
+struct RefreshTokenRequest: Codable {
+    let refreshToken: String
+}
