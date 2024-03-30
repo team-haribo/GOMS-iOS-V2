598edfd5bc2a0cee324a32a15608fb4d01da5ccf
새미 <s22058@gsm.hs.kr>
2024-03-30 23:34:07 +0900

    ✨ :: Add SignInRequest

---
+++ Projects/Service/Sources/Request/Auth/SignIn/SignInRequest.swift
@@ -0,0 +1,14 @@
+//
+//  SignInRequest.swift
+//  Service
+//
+//  Created by 새미 on 3/30/24.
+//  Copyright © 2024 HARIBO. All rights reserved.
+//
+
+import Foundation
+
+struct SignInRequest: Codable {
+    let email: String
+    let password: String
+}
