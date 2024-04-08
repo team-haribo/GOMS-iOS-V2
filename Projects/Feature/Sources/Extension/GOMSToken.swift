c70e5edf10be0a118eaa76b182af747605a7a246
새미 <s22058@gsm.hs.kr>
2024-04-08 11:03:08 +0900

    ✨ :: Add KeyChain read

--- Projects/Feature/Sources/Extension/GOMSToken.swift
+++ Projects/Feature/Sources/Extension/GOMSToken.swift
@@ -11,7 +11,7 @@ import Service
 import Security
 import Foundation
 
-class Keychain {
+class KeyChain {
     func create(key: String, token: String) {
         let query: NSDictionary = [
             kSecClass: kSecClassGenericPassword,
@@ -23,12 +23,26 @@ class Keychain {
         assert(status == noErr, "failed to save Token")
     }
     
-    func read() {
+    func read(key: String) -> String? {
+        let query: NSDictionary = [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: key,
+            kSecReturnData: kCFBooleanTrue as Any,
+            kSecMatchLimit: kSecMatchLimitOne
+        ]
         
-    }
-    
-    func update() {
+        var dataTypeRef: AnyObject?
+        let status = SecItemCopyMatching(query, &dataTypeRef)
         
+        if status == errSecSuccess {
+            if let retrievedData: Data = dataTypeRef as? Data {
+                let value = String(data: retrievedData, encoding: String.Encoding.utf8)
+                return value
+            } else { return nil }
+        } else {
+            print("failed to loading, status code = \(status)")
+            return nil
+        }
     }
     
     func delete() {
