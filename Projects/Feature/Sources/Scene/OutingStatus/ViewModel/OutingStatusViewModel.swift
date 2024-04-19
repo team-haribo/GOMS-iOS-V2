8ac60d2a6b8d0792d9a60b9b31490ad25629aa0b
새미 <s22058@gsm.hs.kr>
2024-04-19 09:56:35 +0900

    🚚 :: OutingServices 폴더 이동

---
+++ Projects/Feature/Sources/Scene/OutingStatus/ViewModel/OutingStatusViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  OutingStatusViewModel.swift
+//  Feature
+//
+//  Created by 새미 on 4/19/24.
+//  Copyright © 2024 HARIBO. All rights reserved.
+//
+
+import Moya
+import Service
+
+public final class OutingStatusViewModel {
+    private let authProvider = MoyaProvider<OutingServices>()
+
+}
