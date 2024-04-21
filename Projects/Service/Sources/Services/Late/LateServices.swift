41a48d6b26e0ff87628b9899ab0997c04de5c254
MelBIP <s22005@gsm.hs.kr>
2024-04-22 00:17:18 +0900

    🔨 :: LateServices

--- Projects/Service/Sources/Services/Late/LateServices.swift
+++ Projects/Service/Sources/Services/Late/LateServices.swift
@@ -2,12 +2,12 @@ import Foundation
 import Moya
 
 public enum LateServices {
-    case lateRank(param: LateRankRequest)
+    case lateRank(authorization: String)
 }
 
 extension LateServices: TargetType {
     public var baseURL: URL {
-        return URL(string: BaseURL.baseURL) ?? URL(string: "https://port-0-goms-backend-v2-duzu222alg58k27h.sel3.cloudtype.app/api/v2")!
+        return URL(string: "https://port-0-goms-backend-v2-duzu222alg58k27h.sel3.cloudtype.app/api/v2")!
     }
     
     public var path: String {
@@ -37,6 +37,8 @@ extension LateServices: TargetType {
     
     public var headers: [String : String]? {
         switch self {
+        case .lateRank(let authorization):
+            return["Content-Type" :"application/json","Authorization" : authorization]
         default:
             return["Content-Type" :"application/json"]
         }
