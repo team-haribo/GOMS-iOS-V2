7d843fe985cad6f0425439e53eae457657a1a29d
새미 <s22058@gsm.hs.kr>
2024-04-21 17:02:49 +0900

    ✨ :: Add AccountServices

--- Projects/Service/Sources/Services/Account/AccountServices.swift
+++ Projects/Service/Sources/Services/Account/AccountServices.swift
@@ -10,5 +10,45 @@ import Foundation
 import Moya
 
 public enum AccountServices {
-   
+    case newPassword(param: NewPasswordRequest, authorization: String)
+}
+
+extension AccountServices: TargetType {
+    public var baseURL: URL {
+        return URL(string: "https://port-0-goms-backend-v2-duzu222alg58k27h.sel3.cloudtype.app/api/v2")!
+    }
+    
+    public var path: String {
+        switch self {
+        case .newPassword:
+            return "/account/new-password"
+        }
+    }
+    
+    public var method: Moya.Method {
+        switch self {
+        case .newPassword:
+            return .patch
+        }
+    }
+    
+    public var sampleData: Data {
+        return "@@".data(using: .utf8)!
+    }
+    
+    public var task: Task {
+        switch self {
+        case .newPassword(let param, _):
+            return .requestJSONEncodable(param)
+        }
+    }
+    
+    public var headers: [String : String]? {
+        switch self {
+        case .newPassword(_, let authorization):
+            return ["Content-Type": "application/json", "Authorization": authorization]
+        default:
+            return ["Content-Type": "application/json"]
+        }
+    }
 }
