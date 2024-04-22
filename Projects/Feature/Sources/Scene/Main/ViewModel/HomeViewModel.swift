91e3cdd5ca5a53ee939e04a2fb759aa7bb7acbda
새미 <s22058@gsm.hs.kr>
2024-04-22 09:21:48 +0900

    ✨ :: Get Latecomer Top 3

---
+++ Projects/Feature/Sources/Scene/Main/ViewModel/HomeViewModel.swift
@@ -0,0 +1,60 @@
+//
+//  HomeViewModel.swift
+//  Feature
+//
+//  Created by 새미 on 4/22/24.
+//  Copyright © 2024 HARIBO. All rights reserved.
+//
+
+import Moya
+import Service
+import Foundation
+
+struct LatecomerData {
+    let profileImageURL: String?
+    let name: String
+    let grade: Int
+    let major: String
+}
+
+public final class HomeViewModel {
+    private let lateProvider = MoyaProvider<LateService>()
+    
+    let keyChain = KeyChain()
+    let gomsRefreshToken = GOMSRefreshToken.shared
+    lazy var accessToken = "Bearer " + (keyChain.read(key: Const.KeyChainKey.accessToken) ?? "")
+    
+    var lateList: [LatecomerResponse] = []
+    var lateListDatas: [LatecomerData] = []
+    
+    func getLateList(completion: @escaping () -> Void) {
+        lateProvider.request(.lateRank(authorization: accessToken)) { response in
+            switch response {
+            case .success(let result):
+                let responseData = result.data
+                do {
+                    self.lateList = try JSONDecoder().decode([LatecomerResponse].self, from: responseData)
+                    self.lateListDatas = self.lateList.map { LatecomerData(profileImageURL: $0.profileUrl, name: $0.name, grade: $0.grade, major: $0.major) }
+                    completion()
+                } catch(let err) {
+                    print(String(describing: err))
+                }
+                let statusCode = result.statusCode
+                switch statusCode {
+                case 200:
+                    print("OK")
+                case 401:
+                    self.gomsRefreshToken.tokenReissuance()
+                case 404:
+                    print("지각자 없음")
+                case 500:
+                    print("SERVER ERROR")
+                default:
+                    print(result)
+                }
+            case .failure(let err):
+                print(err.localizedDescription)
+            }
+        }
+    }
+}
