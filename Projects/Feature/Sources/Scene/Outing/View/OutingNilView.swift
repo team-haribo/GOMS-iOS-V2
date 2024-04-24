d403671bb9eeecab4bae47a26a77eabaaa19af89
새미 <s22058@gsm.hs.kr>
2024-04-24 20:54:46 +0900

    💄 :: Add OutingNilView

--- Projects/Feature/Sources/Scene/Outing/View/OutingNilView.swift
+++ Projects/Feature/Sources/Scene/Outing/View/OutingNilView.swift
@@ -8,25 +8,44 @@
 
 import UIKit
 
+import SnapKit
+import Then
+
 class OutingNilView: UIView {
     
-    // MARK: - Properties
+    private let coffeeIcon = UIImageView()
     
-
-    // MARK: - Initializer
+    private let mainLabel = UILabel().then {
+        $0.text = "텅 비어있네요... 다들 바쁜가 봐요!"
+        $0.textColor = .color.gomsTextDefault.color
+        $0.font = UIFont.pretendard(size: 14, weight: .semibold)
+    }
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureUI()
-        addView()
-        setLayout()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setLayout() {
+    private func configureUI() {
+        coffeeIcon.image = .image.coffee.image
         
+        [coffeeIcon, mainLabel].forEach { self.addSubview($0) }
+        
+        coffeeIcon.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(16)
+            $0.top.bottom.equalToSuperview().inset(8)
+            $0.centerY.equalToSuperview()
+        }
+        
+        mainLabel.snp.makeConstraints {
+            $0.leading.equalTo(coffeeIcon.snp.trailing).offset(4)
+            $0.top.bottom.equalToSuperview().inset(8)
+            $0.centerY.equalToSuperview()
+        }
     }
 
 }
