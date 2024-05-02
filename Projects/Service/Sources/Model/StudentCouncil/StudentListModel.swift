73c6e66e511ecc31209a239c8d91db6601fc539d
새미 <s22058@gsm.hs.kr>
2024-05-02 20:39:44 +0900

    ✨ :: Add StudentListModel

---
+++ Projects/Service/Sources/Model/StudentCouncil/StudentListModel.swift
@@ -0,0 +1,24 @@
+//
+//  StudentListModel.swift
+//  Service
+//
+//  Created by 새미 on 5/2/24.
+//  Copyright © 2024 HARIBO. All rights reserved.
+//
+
+import Foundation
+
+public struct StudentListModel: Codable {
+    let data: StudentListResponse
+}
+
+public struct StudentListResponse: Codable {
+    public let accountIdx: UUID
+    public let name: String
+    public let major: String
+    public let grade: Int
+    public let gender: String
+    public let profileUrl: String?
+    public let authority: String
+    public let isBlackList: Bool
+}
