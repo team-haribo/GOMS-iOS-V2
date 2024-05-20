37a63247e30a80bed6c291cec47c85ce804fd3f8
새미 <s22058@gsm.hs.kr>
2024-05-20 20:20:37 +0900

    💄 :: User Outing NavBar 수정

--- Projects/Feature/Sources/Scene/Outing/User/View/OutingViewController.swift
+++ Projects/Feature/Sources/Scene/Outing/User/View/OutingViewController.swift
@@ -66,6 +66,7 @@ public final class OutingViewController: BaseViewController {
     
     // MARK: - Setting
     override func configNavigation() {
+        self.navigationController?.navigationBar.isHidden = false
         navigationController?.navigationBar.prefersLargeTitles = true
         navigationItem.title = "외출 현황"
         navigationItem.searchController = searchController
