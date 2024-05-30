cd3c00aacefa0a5f6eeca42f314a38ebfc9676a6
새미 <s22058@gsm.hs.kr>
2024-05-30 20:37:25 +0900

    💄 :: Edit Withdraw Layout

--- Projects/Feature/Sources/Scene/Profile/Controller/WithdrawalViewController.swift
+++ Projects/Feature/Sources/Scene/Profile/Controller/WithdrawalViewController.swift
@@ -8,17 +8,51 @@
 
 import UIKit
 
-public class WithdrawalViewController: BaseViewController {
+public class WithdrawalViewController: BaseViewController, UITextFieldDelegate {
 
     // MARK: - Properties
     let passwordTextField = GOMSTextField(frame: CGRect(x: 0, y: 0, width: 0, height: 0), placeholder: "현재 비밀번호")
+    
+    lazy var visiblePasswordButton = UIButton().then {
+        $0.setImage(.image.visible.image, for: .normal)
+        $0.addTarget(self, action: #selector(visiblePasswordButtonTapped), for: .touchUpInside)
+    }
     
     private lazy var withdrawalButton = GOMSButton(frame: CGRect(x: 0, y: 0, width: 0, height: 0), title: "회원 탈퇴하기")
     
     // MARK: - Life Cycel
     public override func viewDidLoad() {
         super.viewDidLoad()
+        
+        passwordTextField.delegate = self
+    }
+    
+    // MARK: - Selectors
+    @objc func visiblePasswordButtonTapped() {
+        passwordTextField.isSecureTextEntry.toggle()
+        passwordTextField.isSelected.toggle()
+        
+        if passwordTextField.isSelected {
+            visiblePasswordButton.setImage(.image.invisible.image, for: .normal)
+        } else {
+            visiblePasswordButton.setImage(.image.visible.image, for: .normal)
+        }
+    }
+    
+    @objc override func keyboardWillShow(_ sender: Notification) {
+        withdrawalButton.snp.remakeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.top.equalTo(-bounds.height * 0.41)
+            $0.height.equalTo(48)
+        }
+    }
 
+    @objc override func keyboardWillHide(_ sender: Notification) {
+        withdrawalButton.snp.remakeConstraints {
+            $0.leading.trailing.equalToSuperview().inset(20)
+            $0.bottom.equalTo(-bounds.height * 0.16)
+            $0.height.equalTo(48)
+        }
     }
     
     // MARK: - Configure Navigation
