6c1b2e5977f0f99b902c55ad7cfeb933328f91fd
saemiee <s22058@gsm.hs.kr>
2024-06-15 21:04:55 +0900

    💄 :: Delete Ouser Outing ScrollView

--- Projects/Feature/Sources/Scene/Outing/User/View/OutingViewController.swift
+++ Projects/Feature/Sources/Scene/Outing/User/View/OutingViewController.swift
@@ -21,14 +21,6 @@ public final class OutingViewController: BaseViewController {
     
     let refreshControl = UIRefreshControl()
     
-    let scrollView = UIScrollView().then {
-        $0.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
-    let contentView1 = UIView().then {
-        $0.translatesAutoresizingMaskIntoConstraints = false
-    }
-    
     private let searchController = UISearchController(searchResultsController: nil)
     
     private let searchTitle = UILabel().then {
@@ -107,20 +99,11 @@ public final class OutingViewController: BaseViewController {
     }
     
     private func setupScrollView() {
-        view.addSubview(scrollView)
-        scrollView.addSubview(contentView1)
-        scrollView.snp.makeConstraints { make in
-            make.edges.equalToSuperview()
-        }
-        contentView1.snp.makeConstraints { make in
-            make.edges.equalTo(scrollView)
-            make.width.equalTo(scrollView)
-            make.bottom.equalTo(outingListCollectionView.snp.bottom)
-        }
         addView()
         configureRefreshControl()
     }
-        // MARK: - Refresh Control Setup
+    
+    // MARK: - Refresh Control Setup
     private func configureRefreshControl() {
         outingListCollectionView.refreshControl = refreshControl
         refreshControl.addTarget(self, action: #selector(handleRefreshControl), for: .valueChanged)
