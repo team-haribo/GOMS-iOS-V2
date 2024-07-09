0759043380dd7d82b9ac3ad42f7263db109661d2
새미 <saemi@saemiui-MacBookAir.local>
2024-07-09 09:18:23 +0900

    💄 ;; Outing Success VC

---
+++ Projects/Feature/Sources/Scene/QRCode/QR/CombackScuccessVC.swift
@@ -0,0 +1,30 @@
+//
+//  CombackScuccessVC.swift
+//  Feature
+//
+//  Created by 새미 on 7/9/24.
+//  Copyright © 2024 HARIBO. All rights reserved.
+//
+
+import UIKit
+
+class CombackScuccessVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Projects/Feature/Sources/Scene/QRCode/User/QRCodeViewController.swift
+++ Projects/Feature/Sources/Scene/QRCode/User/QRCodeViewController.swift
@@ -79,8 +79,8 @@ public class QRCodeViewController: BaseViewController, AVCaptureVideoDataOutputS
             title = "QR코드 스캔 성공"
             message = "제 시간에 복귀에 성공했어요!\n다음 외출제에 또 만나요!"
         } else if result == "outing" {
-            title = "QR코드 스캔 성공"
-            message = "외출을 시작합니다.\n7시 25분까지 복귀해 주세요."
+            let vc = OutingSuccessVC()
+            self.navigationController?.pushViewController(vc, animated: true)
         } else if result == "blackList" {
             title = "QR코드 스캔 실패"
             message = "외출 금지 상태에서는\nQR 스캔을 할 수 없습니다."
@@ -89,13 +89,13 @@ public class QRCodeViewController: BaseViewController, AVCaptureVideoDataOutputS
             message = "예기치 못한 오류가 발생했습니다.\n다시 시도해 주세요."
         }
         
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let action = UIAlertAction(title: "확인", style: .default) { _ in
-            let mainVC = MainViewController()
-            self.navigationController?.pushViewController(mainVC, animated: true)
-        }
-        alert.addAction(action)
-        self.present(alert, animated: true, completion: nil)
+//        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+//        let action = UIAlertAction(title: "확인", style: .default) { _ in
+//            let mainVC = MainViewController()
+//            self.navigationController?.pushViewController(mainVC, animated: true)
+//        }
+//        alert.addAction(action)
+//        self.present(alert, animated: true, completion: nil)
     }
     
     public func setupCamera() {
