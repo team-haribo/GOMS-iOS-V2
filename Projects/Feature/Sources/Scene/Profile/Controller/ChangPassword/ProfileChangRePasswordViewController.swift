a2bc61cc86f0d01b109d53f9f3e42ed99c4b0be2
새미 <saemi@saemiui-MacBookAir.local>
2024-07-16 08:41:11 +0900

    ✨ :: 비밀번호 재설정 스페이스 제한

--- Projects/Feature/Sources/Scene/Profile/Controller/ChangPassword/ProfileChangRePasswordViewController.swift
+++ Projects/Feature/Sources/Scene/Profile/Controller/ChangPassword/ProfileChangRePasswordViewController.swift
@@ -81,6 +81,8 @@ public class ProfileChangRePasswordViewController: BaseViewController, UIImagePi
     public override func viewDidLoad() {
         super.viewDidLoad()
         
+        passwordTextField.delegate = self
+        
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
@@ -158,3 +160,17 @@ public class ProfileChangRePasswordViewController: BaseViewController, UIImagePi
         }
     }
 }
+
+extension ProfileChangRePasswordViewController: UITextFieldDelegate {
+    
+    public func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        let currentText = (textField.text ?? "") as NSString
+        let updatedText = currentText.replacingCharacters(in: range, with: string)
+        
+        if updatedText.rangeOfCharacter(from: .whitespaces) != nil { return false }
+        
+        return true
+    }
+}
+
+
