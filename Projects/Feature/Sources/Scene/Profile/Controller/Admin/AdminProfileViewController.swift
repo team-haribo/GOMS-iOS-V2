336573305c1233d4b5256046a722c01024324afb
“xixn2” <ssbkh0802@naver.com>
2024-07-21 01:26:45 +0900

    ♻️ :: [#258] Edit AdminProfileVC Variable themesetting -> themeSetting

--- Projects/Feature/Sources/Scene/Profile/Controller/Admin/AdminProfileViewController.swift
+++ Projects/Feature/Sources/Scene/Profile/Controller/Admin/AdminProfileViewController.swift
@@ -85,13 +85,13 @@ public class AdminProfileViewController: BaseViewController,UIImagePickerControl
         $0.layer.borderWidth = 1.0
     }
     
-    let themesettingText = UILabel().then {
+    let themeSettingText = UILabel().then {
         $0.text = ""
         $0.textColor = .color.gomsSecondary.color
         $0.font = .pretendard(size: 16, weight: .regular)
     }
     
-    let themesettingImg = UIImageView().then {
+    let themeSettingImg = UIImageView().then {
         $0.image = .image.gomsBottomButton.image
     }
     
