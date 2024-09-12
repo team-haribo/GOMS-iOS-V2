64cd99358aea762081aa66870489d48c4c5823e0
saemiee <s22058@gsm.hs.kr>
2024-09-12 20:03:03 +0900

    ✨ :: Add Major Data enum

---
+++ Projects/Feature/Sources/Data/Major.swift
@@ -0,0 +1,15 @@
+//
+//  Major.swift
+//  Feature
+//
+//  Created by 새미 on 9/12/24.
+//  Copyright © 2024 HARIBO. All rights reserved.
+//
+
+import Foundation
+
+enum Major: String {
+    case sw = "SW_DEVELOP"
+    case iot = "SMART_IOT"
+    case ai = "AI"
+}
