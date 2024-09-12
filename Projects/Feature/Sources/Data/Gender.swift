aa8cb3dee0e68e06d4446e723db95e04c859fb52
saemiee <s22058@gsm.hs.kr>
2024-09-12 20:04:23 +0900

    ✨ :: Add Gender Data enum

---
+++ Projects/Feature/Sources/Data/Gender.swift
@@ -0,0 +1,14 @@
+//
+//  Gender.swift
+//  Feature
+//
+//  Created by 새미 on 9/12/24.
+//  Copyright © 2024 HARIBO. All rights reserved.
+//
+
+import Foundation
+
+enum Gender: String {
+    case man = "MAN"
+    case woman = "WOMAN"
+}
