ac4df40413a8da1ab62c331e696b37256b6c63bf
“xixn2” <ssbkh0802@naver.com>
2024-11-17 15:46:15 +0900

    ⚡ :: [#270] Edit MainVC Srcoll Refresh Range All

--- Projects/Feature/Sources/Scene/Main/Cell/OutingStatusCollectionViewCell.swift
+++ Projects/Feature/Sources/Scene/Main/Cell/OutingStatusCollectionViewCell.swift
@@ -13,22 +13,22 @@ import Then
 import Kingfisher
 
 final class OutingStatusCollectionViewCell: UICollectionViewCell {
-    
+
     // MARK: - Properties
     static let identifier = "OutingStatusCell"
-    
+
     let profileImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 28, height: 28))
-    
+
     let nameLabel = UILabel().then {
         $0.textColor = .color.gomsSecondary.color
         $0.font = UIFont.pretendard(size: 16, weight: .semibold)
     }
-    
+
     let studentInfoLabel = UILabel().then {
         $0.textColor = .color.gomsTertiary.color
         $0.font = UIFont.pretendard(size: 12, weight: .regular)
     }
-    
+
     // MARK: - Initializer
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -36,12 +36,17 @@ final class OutingStatusCollectionViewCell: UICollectionViewCell {
         addView()
         setLayout()
     }
-    
+
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    func setupData(with outingData: OutingListData) {
+
+    // MARK: - Configure
+    func configure(with data: OutingListData) {
+        setupData(with: data)
+    }
+
+    public func setupData(with outingData: OutingListData) {
         if let imageURL = outingData.profileImageURL, let url = URL(string: imageURL) {
             profileImageView.kf.setImage(with: url, placeholder: UIImage(systemName: "person.crop.circle.fill"))
         } else {
@@ -56,18 +61,18 @@ final class OutingStatusCollectionViewCell: UICollectionViewCell {
             studentInfoLabel.text = "\(outingData.grade)기 | AI"
         }
     }
-    
+
     // MARK: - Configure UI
     private func configureUI() {
         profileImageView.layer.cornerRadius = profileImageView.frame.size.width / 2
         profileImageView.clipsToBounds = true
     }
-    
+
     // MARK: - Add View
     private func addView() {
         [profileImageView, nameLabel, studentInfoLabel].forEach { contentView.addSubview($0) }
     }
-    
+
     // MARK: - Layout
     private func setLayout() {
         profileImageView.snp.makeConstraints {
@@ -75,13 +80,13 @@ final class OutingStatusCollectionViewCell: UICollectionViewCell {
             $0.leading.equalToSuperview().inset(16)
             $0.top.bottom.equalToSuperview().inset(11)
         }
-        
+
         nameLabel.snp.makeConstraints {
             $0.height.equalTo(28)
             $0.leading.equalTo(profileImageView.snp.trailing).offset(8)
             $0.centerY.equalToSuperview()
         }
-        
+
         studentInfoLabel.snp.makeConstraints {
             $0.height.equalTo(20)
             $0.centerY.equalToSuperview()
