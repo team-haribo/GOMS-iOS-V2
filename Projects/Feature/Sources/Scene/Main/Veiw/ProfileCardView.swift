119c3b9d80025bb8c720af8be05244182003cb4e
“xixn2” <ssbkh0802@naver.com>
2024-11-23 14:15:27 +0900

    🎨 :: [#282] Edit myOutingStatusLabel, profileStatus String 초기화

--- Projects/Feature/Sources/Scene/Main/Veiw/ProfileCardView.swift
+++ Projects/Feature/Sources/Scene/Main/Veiw/ProfileCardView.swift
@@ -32,7 +32,7 @@ final class ProfileCardView: UIView {
     }
 
     let myOutingStatusLabel = UILabel().then {
-        $0.text = "학생회"
+        $0.text = ""
         $0.textColor = .color.gomsAdmin.color
         $0.font = UIFont.pretendard(size: 16, weight: .bold)
     }
