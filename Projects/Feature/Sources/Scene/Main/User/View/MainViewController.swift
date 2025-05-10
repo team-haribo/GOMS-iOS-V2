e8825c9458ab261d279097bab833957fd36538c7
“xixn2” <ssbkh0802@naver.com>
2025-05-10 16:44:35 +0900

    🎨 :: [#298] 메인VC에도 DispatchGroup 병렬 처리 적용

--- Projects/Feature/Sources/Scene/Main/User/View/MainViewController.swift
+++ Projects/Feature/Sources/Scene/Main/User/View/MainViewController.swift
@@ -227,38 +227,36 @@ public final class MainViewController: BaseViewController, UICollectionViewDataS
     }
 
     private func fetchData() {
+        let group = DispatchGroup()
+
+        group.enter()
         mainViewModel.getLateList { [weak self] in
-            guard let self = self else { return }
+            group.leave()
+        }
 
-            self.mainViewModel.getProfile { [weak self] _ in
-                guard let self = self else { return }
+        group.enter()
+        mainViewModel.getProfile { [weak self] _ in
+            group.leave()
+        }
 
-                DispatchQueue.main.async {
-                    guard self.isVisible else {
-                        self.refreshControl.endRefreshing()
-                        return
-                    }
+        group.enter()
+        mainViewModel.getOutingList { [weak self] in
+            group.leave()
+        }
 
-                    self.setupProfileView()
-                    self.mainViewModel.getOutingList { [weak self] in
-                        guard let self = self else { return }
-
-                        DispatchQueue.main.async {
-                            guard self.isVisible else {
-                                self.refreshControl.endRefreshing()
-                                return
-                            }
-
-                            self.setupViewComponents()
-                            self.latecomerCollectionView.reloadData()
-                            self.outingStatusCollectionView.reloadData()
-                            self.refreshControl.endRefreshing()
-                        }
-                    }
-                }
+        group.notify(queue: .main) { [weak self] in
+            guard let self = self, self.isVisible else {
+                self?.refreshControl.endRefreshing()
+                return
             }
+            self.setupProfileView()
+            self.setupViewComponents()
+            self.latecomerCollectionView.reloadData()
+            self.outingStatusCollectionView.reloadData()
+            self.refreshControl.endRefreshing()
         }
     }
+
 
     private func setupViewComponents() {
         self.setup()
